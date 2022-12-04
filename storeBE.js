95eb41718e4fa3e2b45e10ab5717b26b921ae643
nishabdShunya <nishantchoubisa92@gmail.com>
2022-12-05 04:30:46 +0530

    frontend code for add to cart functionality using backend

--- storeBE.js
+++ storeBE.js
@@ -16,59 +16,8 @@ cartXBtn.addEventListener('click', (event) => {
     cartContainer.style.display = 'none';
 })
 
-// Adding to the Cart (Used Event Bubbling)
-const music = document.getElementById('music');
-music.addEventListener('click', (event) => {
-    if (event.target.innerText === 'Add To Cart') {
-        addToCart(event.target);
-    }
-});
+// const cart = document.getElementById('cart');
 
-const merch = document.getElementById('merch');
-merch.addEventListener('click', (event) => {
-    if (event.target.innerText === 'Add To Cart') {
-        addToCart(event.target);
-    }
-});
-
-const cart = document.getElementById('cart');
-
-function addToCart(addToCartBtn) {
-    const item = addToCartBtn.parentElement.parentElement;
-    const itemTitle = item.querySelector('.item-title').innerText;
-    const itemImgSrc = item.querySelector('.item-img').src;
-    const itemPrice = item.querySelector('.item-price').innerText;
-    const cartItemsTitles = cart.getElementsByClassName('cart-item-title');
-    for (let title of cartItemsTitles) {
-        if (title.innerText === itemTitle) {
-            alert('Item already added to the cart.');
-            return;
-        }
-    }
-    const cartItem = `
-        <li class="cart-item">
-            <ul>
-                <li><img src="${itemImgSrc}" alt="${itemTitle}"></li>
-                <li class = "cart-item-title">${itemTitle}</li>
-                <li class = "cart-item-price">${itemPrice}</li>
-                <li><input type="number" name="item-quantity" class="cart-item-quantity" value="1"></li>
-                <li><button class="remove-btn">REMOVE</button></li>
-            </ul>
-        </li>`;
-    cart.innerHTML += cartItem;
-    updateTotal();
-    showNotification();
-    // Removing Items from the Cart
-    const removeBtns = cart.getElementsByClassName('remove-btn');
-    for (let btn of removeBtns) {
-        btn.addEventListener('click', removeItem);
-    }
-    // Effect of changing quantity of the Items
-    const quantityInputs = cart.getElementsByClassName('cart-item-quantity');
-    for (let quantity of quantityInputs) {
-        quantity.addEventListener('change', quantityChanged);
-    }
-};
 
 function removeItem(event) {
     event.target.parentElement.parentElement.parentElement.remove();
@@ -120,14 +69,16 @@ document.getElementById('purchase-btn').addEventListener('click', (event) => {
     updateTotal();
 });
 
-// 
+// BACKEND
+const productList = document.getElementById('product-list');
+const cart = document.getElementById('cart');
+
 window.addEventListener('DOMContentLoaded', (event) => {
     axios.get('http://localhost:3000/products')
         .then(response => {
             const products = response.data;
             for (let product of products) {
-                const musicList = document.getElementById('music-list');
-                const musicListItem = `
+                const productListItem = `
                 <li>
                     <h3 class="item-title">${product.title}</h3>
                     <div class="img-container">
@@ -135,11 +86,36 @@ window.addEventListener('DOMContentLoaded', (event) => {
                     </div>
                     <div class="add-to-cart-container">
                         <p class="item-price"><span>$</span>${product.price}</p>
-                        <button class="section-btns">Add To Cart</button>
+                        <button class="section-btns" type="submit" onClick="addToCart('${product.id}', '${product.title}', '${product.imageUrl}', '${product.price}')">Add To Cart</button>
                     </div>
                 </li>`;
-                musicList.innerHTML += musicListItem;
+                productList.innerHTML += productListItem;
             }
         })
         .catch(err => console.log(err));
 });
+
+function addToCart(id, title, image, price) {
+    const productDetails = {
+        id: id,
+        title: title,
+        image: image,
+        price: price
+    }
+    axios.post('http://localhost:3000/cart', productDetails)
+        .then(response => {
+            const cartProduct = response.data;
+            const cartItem = `
+            <li class="cart-item">
+                <ul>
+                    <li><img src="${cartProduct.image}" alt="${cartProduct.title}"></li>
+                    <li class = "cart-item-title">${cartProduct.title}</li>
+                    <li class = "cart-item-price">${cartProduct.price}</li>
+                    <li><input type="number" name="item-quantity" class="cart-item-quantity" value="1"></li>
+                    <li><button class="remove-btn">REMOVE</button></li>
+                </ul>
+            </li>`;
+            cart.innerHTML += cartItem;
+        })
+        .catch(err => console.log(err));
+}
